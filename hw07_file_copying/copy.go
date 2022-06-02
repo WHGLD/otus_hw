2aa5561213f42cf33457882147a4ab54167f73f9
Egor Obukhov <egor.obukhov@x5.ru>
2022-06-02 17:24:31 +0300

    HW7 is complited

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,78 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrOfCopyProcess         = errors.New("cant copy to a new file")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+
+	file, err := getFileToCopyWithOffset(fromPath, offset)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer file.Close()
+
+	// offset больше, чем размер файла - невалидная ситуация
+	fileStat, errStat := file.Stat()
+	if errStat != nil {
+		return ErrUnsupportedFile
+	}
+	fileSize := fileStat.Size()
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if errCopy := copyWithProcessBar(file, toPath, fileSize, limit); errCopy != nil {
+		return ErrOfCopyProcess
+	}
+
+	return nil
+}
+
+func getFileToCopyWithOffset(fromPath string, offset int64) (*os.File, error) {
+	file, errOpen := os.Open(fromPath)
+	if errOpen != nil {
+		return nil, errOpen
+	}
+	_, errSeek := file.Seek(offset, 0)
+	if errSeek != nil {
+		return nil, errSeek
+	}
+
+	return file, nil
+}
+
+func copyWithProcessBar(file *os.File, toPath string, fileSize int64, limit int64) error {
+	barLimit := fileSize
+	reader := io.LimitReader(file, barLimit)
+	bar := pb.Full.Start64(barLimit)
+	bar.Set(pb.Bytes, true)
+	bar.Set(pb.SIBytesPrefix, true)
+	defer bar.Finish()
+
+	newFile, errNew := os.Create(toPath)
+	if errNew != nil {
+		return errNew
+	}
+	defer newFile.Close()
+
+	if limit == 0 {
+		limit = fileSize
+	}
+	barReader := bar.NewProxyReader(reader)
+	_, errCopy := io.CopyN(newFile, barReader, limit)
+	if errCopy != nil && errCopy != io.EOF {
+		return errCopy
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,59 @@
 package main
 
-import "testing"
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	os.Mkdir("tmp", 0755)
+	defer os.RemoveAll("tmp")
+
+	// 1. запускаем без параметров
+	runTestCase(t, 0, 0)
+
+	// 2. проверяем offset
+	runTestCase(t, 10, 0)
+
+	// 3. проверяем limit
+	runTestCase(t, 0, 10)
+
+	// 4. проверяем offset и limit
+	runTestCase(t, 100, 1000)
+}
+
+func runTestCase(t *testing.T, offset, limit int64) {
+	testOutFilePath := fmt.Sprintf("tmp/test_out_%d_%d.txt", offset, limit)
+	correctOutFilePath := fmt.Sprintf("testdata/out_offset%d_limit%d.txt", offset, limit)
+	err := Copy(
+		"testdata/input.txt",
+		testOutFilePath,
+		offset,
+		limit,
+	)
+	require.NoError(t, err)
+	correctOut := getMD5SumString(correctOutFilePath)
+	testOut := getMD5SumString(testOutFilePath)
+	require.Equal(t, correctOut, testOut)
+}
+
+func getMD5SumString(path string) string {
+	f, err := os.Open(path)
+	if err != nil {
+		return ""
+	}
+
+	file1Sum := md5.New()
+
+	_, err = io.Copy(file1Sum, f)
+	if err != nil {
+		return ""
+	}
+
+	return fmt.Sprintf("%X", file1Sum.Sum(nil))
 }
