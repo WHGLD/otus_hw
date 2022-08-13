c6512b11f36ddc96c12c9eb8e72522e53f2e0c3e
Egor Obukhov <egor.obukhov@x5.ru>
2022-08-13 12:01:07 +0300

    lint fix

--- hw11_telnet_client/telnet_test.go
+++ hw11_telnet_client/telnet_test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bytes"
-	"io/ioutil"
+	"io"
 	"net"
 	"sync"
 	"testing"
@@ -29,7 +29,7 @@ func TestTelnetClient(t *testing.T) {
 			timeout, err := time.ParseDuration("10s")
 			require.NoError(t, err)
 
-			client := NewTelnetClient(l.Addr().String(), timeout, ioutil.NopCloser(in), out)
+			client := NewTelnetClient(l.Addr().String(), timeout, io.NopCloser(in), out)
 			require.NoError(t, client.Connect())
 			defer func() { require.NoError(t, client.Close()) }()
 
@@ -78,7 +78,7 @@ func TestNotFoundServer(t *testing.T) {
 			timeout, err := time.ParseDuration("1s")
 			require.NoError(t, err)
 
-			client := NewTelnetClient("127.0.0.1:15012", timeout, ioutil.NopCloser(in), out)
+			client := NewTelnetClient("127.0.0.1:15012", timeout, io.NopCloser(in), out)
 			require.Error(t, client.Connect())
 		}()
 
